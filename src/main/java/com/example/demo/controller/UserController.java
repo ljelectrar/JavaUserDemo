9119f98a617ffa979bef716db56af9803f2044a9
ljelectrar <ljelectrar@gmail.com>
2024-02-08 20:26:26 -0300

    Getting the Http header

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,14 +3,15 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.PatchUserRequest;
@@ -24,6 +25,13 @@ public class UserController {
 	@Autowired
 	private UserService service;
 	
+	@GetMapping("/test")
+	public String test(@RequestHeader("number") HttpHeaders header) {
+		return header.getHost().getHostName() + " -  " + header.getHost().getPort();
+		
+	}
+	
+	/*
 	@GetMapping("/test/{id}/{name}")
 	public String test(@PathVariable int id, @PathVariable String name) {
 		return id + " " + name;
@@ -34,6 +42,8 @@ public class UserController {
 	public void test2(@RequestParam("id") int id, @RequestParam("name" String name) {
 		System.out.println(id + " " + name);
 	}
+	
+	*/
 	
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable("id")  String userId) {
