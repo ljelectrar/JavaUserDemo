367b947cf4444d2692efb97b0ed420ec6347cb63
ljelectrar <ljelectrar@gmail.com>
2024-02-11 11:01:19 -0300

    Refactoring The Exception Handle Class

--- src/main/java/com/example/demo/exceptions/BaseExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/BaseExceptionHandler.java
@@ -1,10 +1,12 @@
-package com.example.demo.controller;
+package com.example.demo.exceptions;
 
 import java.util.NoSuchElementException;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,10 +15,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import com.example.demo.model.ErrorMessage;
 
 @ControllerAdvice
-public class BaseController {
+public class BaseExceptionHandler{
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	//@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	public ResponseEntity<ErrorMessage> handleNotValidException(MethodArgumentNotValidException e) {
 		var errors = e.getAllErrors();
 
@@ -30,5 +32,21 @@ public class BaseController {
 		message = new ErrorMessage(400, "Bad Request");
 		return new ResponseEntity<ErrorMessage>(message, HttpStatus.BAD_REQUEST);
 	}
+	
+	@ExceptionHandler({NoSuchElementException.class, NumberFormatException.class})
+	//@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	public ResponseEntity<ErrorMessage> handleNotFoundException(Exception e) {
 
+		ErrorMessage message = new ErrorMessage(404, "Not Found");
+		return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler({HttpMessageNotReadableException.class, 
+		HttpRequestMethodNotSupportedException.class})
+	//@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ErrorMessage> handleNotReadableException(Exception e) {
+
+		ErrorMessage message = new ErrorMessage(400, "Bad Request");
+		return new ResponseEntity<ErrorMessage>(message, HttpStatus.BAD_REQUEST);
+	}
 }
